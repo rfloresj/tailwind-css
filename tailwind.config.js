76238232b23ecbe61df68a3f12fb6eea9892d7b4
rflores <104962825+rfloresj@users.noreply.github.com>
2023-10-31 18:35:51 -0600

    Typography-P1

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        xs: "12px",
+        sm: "15px",
+        base: "18px",
+      },
+      colors: {
+        vuejs: "#49e659",
+      },
+    },
   },
   plugins: [],
 };
