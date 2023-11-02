1e96de5e6346aa23bfa62b84bbc21bb05f638ea5
rflores <104962825+rfloresj@users.noreply.github.com>
2023-11-01 22:25:37 -0600

    Typography:P2

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 export default function Home() {
   return (
     <div>
-      <h1
-        className="text-3xl italic underline decoration-red-500 
-      decoration-wavy"
-      >
-        Title 1
-      </h1>
+      <p className="break-all">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+        quidemsdfsdfsdfsdfsdfsdfdsfsdfdsfsdfsd suscipit hic sed, repellat
+        corporis in praesentium minus atque numquam.
+      </p>
+      {/* <h1 className="text-3xl">Title 1</h1>
       <h2 className="text-2xl">Title 2</h2>
       <h3 className="text-xl">Title 3</h3>
-      <p className="text-base">A regular paragraph</p>
+      <p className="text-base uppercase">a regular paragraph</p>
       <p className="text-sm">A description paragraph</p>
-      <p className="text-xs note">A little note</p>
+      <p className="text-xs note">A little note</p> */}
     </div>
   );
 }
