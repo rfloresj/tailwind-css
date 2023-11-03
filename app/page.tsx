6314318d5b59e2a56c02c21d1cb5bcf48416b323
rflores <104962825+rfloresj@users.noreply.github.com>
2023-11-02 20:54:51 -0600

    spacing-sizing

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,8 @@
 export default function Home() {
   return (
-    <div>
-      <p className="break-all">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
-        quidemsdfsdfsdfsdfsdfsdfdsfsdfdsfsdfsd suscipit hic sed, repellat
-        corporis in praesentium minus atque numquam.
-      </p>
-      {/* <h1 className="text-3xl">Title 1</h1>
-      <h2 className="text-2xl">Title 2</h2>
-      <h3 className="text-xl">Title 3</h3>
-      <p className="text-base uppercase">a regular paragraph</p>
-      <p className="text-sm">A description paragraph</p>
-      <p className="text-xs note">A little note</p> */}
+    <div className="flex-col min-h-screen space-y-4">
+      <div className="w-1/5 bg-red-500">Sidebar</div>
+      <div className="w-4/5 bg-green-500">Main Content</div>
     </div>
   );
 }
