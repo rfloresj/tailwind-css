2488ca2dc15a2475b3644641240380f344795566
rflores <104962825+rfloresj@users.noreply.github.com>
2023-11-08 13:17:22 -0600

    Flexbox & Grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
 export default function Home() {
   return (
-    <div className="Parent">
-      <div>Header</div>
-      <div className="flex items-baseline">
-        <div className="bg-sky-500 pt-2 pb-6">01</div>
-        <div className="pt-8 pb-12 bg-red-500">02</div>
-        <div className="pt-12 pb-4 bg-green-500">03</div>
+    <div className="h-screen text-white Parent bg-slate-900">
+      <div className="container mx-auto">
+        <div className="grid grid-rows-4 grid-flow-col gap-4">
+          <div className="p-6 bg-sky-500 rounded-lg">01</div>
+          <div className="p-6 bg-sky-500 rounded-lg">02</div>
+          <div className="p-6 bg-sky-500 rounded-lg">03</div>
+          <div className="p-6 bg-sky-500 rounded-lg">04</div>
+          <div className="p-6 bg-sky-500 rounded-lg">05</div>
+          <div className="p-6 bg-sky-500 rounded-lg">06</div>
+          <div className="p-6 bg-sky-500 rounded-lg">07</div>
+          <div className="p-6 bg-sky-500 rounded-lg">08</div>
+          <div className="p-6 bg-sky-500 rounded-lg">09</div>
+          <div className="p-6 bg-sky-500 rounded-lg">10</div>
+          <div className="p-6 bg-sky-500 rounded-lg">11</div>
+          <div className="p-6 bg-sky-500 rounded-lg">12</div>
+        </div>
       </div>
     </div>
   );
