529d73401e6987ba8977cd377f5fcddb3523ff9f
rflores <104962825+rfloresj@users.noreply.github.com>
2023-11-14 17:37:10 -0600

    grid-flow-row-dense

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,17 @@ export default function Home() {
   return (
     <div className="h-screen text-white Parent bg-slate-900">
       <div className="container mx-auto">
-        <div className="grid grid-rows-4 grid-flow-col gap-4">
-          <div className="p-6 bg-sky-500 rounded-lg">01</div>
-          <div className="p-6 bg-sky-500 rounded-lg">02</div>
+        <div className="grid grid-flow-row-dense grid-cols-3 grid-rows-3 gap-4">
+          <div className="p-6 bg-sky-500 rounded-lg col-span-2">01</div>
+          <div className="p-6 bg-sky-500 rounded-lg col-span-2">02</div>
           <div className="p-6 bg-sky-500 rounded-lg">03</div>
           <div className="p-6 bg-sky-500 rounded-lg">04</div>
           <div className="p-6 bg-sky-500 rounded-lg">05</div>
-          <div className="p-6 bg-sky-500 rounded-lg">06</div>
+          <div className="p-6 bg-sky-500 rounded-lg col-span-2">06</div>
           <div className="p-6 bg-sky-500 rounded-lg">07</div>
           <div className="p-6 bg-sky-500 rounded-lg">08</div>
           <div className="p-6 bg-sky-500 rounded-lg">09</div>
-          <div className="p-6 bg-sky-500 rounded-lg">10</div>
+          <div className="p-6 bg-sky-500 rounded-lg col-span-2">10</div>
           <div className="p-6 bg-sky-500 rounded-lg">11</div>
           <div className="p-6 bg-sky-500 rounded-lg">12</div>
         </div>
